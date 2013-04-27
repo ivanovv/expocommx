43c60b0c3214f52ca4c79ff3e7210ae7502d3f65
Victor Ivanov <vic.ivanoff@gmail.com>
2013-04-27 19:19:54 +0400

    Initial Commit

---
+++ expocommx/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  expocommx
+//
+//  Created by Victor Ivanov on 4/27/13.
+//  Copyright (c) 2013 Victor Ivanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "expocommAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([expocommAppDelegate class]));
+    }
+}
